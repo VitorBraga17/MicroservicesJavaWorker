ccf38a4d5b2cb9067ac9e97a32203eab7c6edeb4
joaobragaciandt <joaovitor1702@gmail.com>
2022-09-23 10:22:01 -0300

    hr-payroll implementation mock

---
+++ hr-payrool/src/main/java/com/joaobraga/hrpayrool/entities/Payment.java
@@ -0,0 +1,26 @@
+package com.joaobraga.hrpayrool.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private Double dailyIncome;
+    private Integer days;
+
+    public double getTotal() {
+        return days * dailyIncome;
+    }
+
+
+}
